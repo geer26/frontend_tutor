1466c30273c4ba9c794f3252709816604b767b69
geer26 <gergo.kurucz@gmail.com>
2022-01-17 12:41:05 +0100

    v0.1

--- app/workers.py
+++ app/workers.py
@@ -7,6 +7,26 @@ def adduser(data):
     try:
         user = User()
         user.username = username
+        db.session.add(user)
+        db.session.commit()
         return True
     except(Exception):
         return False
+
+
+def deluser(data):
+    userid = data['userid']
+    try:
+        user = User.query.get(int(userid))
+        # delete all corresponding entries
+        for entry in Entry.query.filter_by(author=user.id).all():
+            db.session.delete(entry)
+        db.session.delete(user)
+        db.session.commit()
+        return True
+    except(Exception):
+        return False
+
+
+def addentry(data):
+    pass
