f5b0b13d63db8620757b5c01c7bebea026b17b5b
geer26 <gergo.kurucz@gmail.com>
2022-01-17 15:35:03 +0100

    v0.1

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 #from flask_login import LoginManager
 from config import Config
 from flask_restful import Api
+from flask_cors import CORS
 #from flask_socketio import SocketIO
 #from roomhandler import Roomhandler
 
@@ -11,24 +12,20 @@ from flask_restful import Api
 app = Flask(__name__)
 app.config.from_object(Config)
 
+CORS(app)
 
 db = SQLAlchemy(app)
 
-
 migrate = Migrate(app, db, render_as_batch=True)
 
-
 #login = LoginManager(app)
-
 
 api = Api(app)
 
 #socket = SocketIO(app)
 #socket.init_app(app, cors_allowed_origins="*")
 
-
 #handler=Roomhandler()
-
 
 #from app import routes, models, workers, endpoints
 from app import models, endpoints
