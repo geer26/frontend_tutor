feda84a4b0b3fd403409e6c0f3fba55d69284c35
geer26 <gergo.kurucz@gmail.com>
2022-01-17 11:04:18 +0100

    v0.1

--- app/__init__.py
+++ app/__init__.py
@@ -31,3 +31,4 @@ api = Api(app)
 
 
 #from app import routes, models, workers, endpoints
+from app import models
---
+++ app/models.py
@@ -0,0 +1,38 @@
+import json, uuid
+from app import db
+import bcrypt
+from datetime import datetime
+#from flask_login import UserMixin
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, index=True, primary_key=True)
+    username = db.Column(db.String(32), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'Username: {self.username}, ID: {self.id}'
+
+    def get_self_json(self):
+        return {
+            'id': self.id,
+            'username': self.username
+        }
+
+
+class Entry(db.Model):
+    id = db.Column(db.Integer, index=True, primary_key=True)
+    title = db.Column(db.String(64), default='No title')
+    body = db.Column(db.String(4096), default='No body')
+    created_at = db.Column(db.Date(), default=datetime.now(), nullable=False)
+    author = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'Username: {self.username}, ID: {self.id}'
+
+    def get_self_json(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'created_at': self.created_at,
+            'author': self.author
+        }
